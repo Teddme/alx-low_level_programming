214ed0b27cc4639a542ec32afdc1e36661c1aa8a
Teddme <tewodrosmeshesha@cbe.com.et>
2022-10-28 19:20:45 +0300

    6&7

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - method changes lowercase to upper
+ * @str: input str
+ *
+ * Return: returns result.
+ */
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
+}
