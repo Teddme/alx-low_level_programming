ecdb960cb1bf2a8eb9cf79f6fb8fe44e87e9fbe2
Teddme <tewodrosmeshesha@cbe.com.et>
2022-10-28 20:49:59 +0300

    101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,8 +4,13 @@
  * print_number - Print int.
  * @n: num printed.
  */
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
 void print_number(int n)
 {
 	unsigned int num = n;
 _putchar(num);
+}
 }
