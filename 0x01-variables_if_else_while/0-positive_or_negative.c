89c7b1ce6d4dcbc897ddaf67ee5a411449ea8de9
Teddme <tewodrosmeshesha@cbe.com.et>
2022-10-30 21:02:48 +0300

    aa

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+	
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
@@ -21,5 +21,5 @@ int main(void)
 	else
 		printf("%d is zero\n", n);
 
-	return (0);
+return (0);
 }
