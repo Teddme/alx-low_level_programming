87083126939c141b8dbcb1f39175e3317bac0859
Teddme <tewodrosmeshesha@cbe.com.et>
2022-10-30 22:08:39 +0300

    All

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - cks is lower
+ * @c: input str
+ *
+ * Return: 1 for lower and 0 for other.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
