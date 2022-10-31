66a42a285868db199e16e2bc1e2ae23da1a0ac41
Tewodros Meshesha <tewodrosmeshesha@cbe.com.et>
2022-10-31 15:02:11 +0300

    Mam

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include<main.h>
+
+/**
+ * _memset -
+ * @s : pt input s
+ * 
+ * @b : pt input b 
+ * 
+ * @n : size of pt 
+
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+	s[i] = b;
+	i++;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+/*
+ * main.h
+ * prototype
+ * used in the 0x05
+ */
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
