db72272edc34b43d2440af0257c4f9b9aa5a5337
Teddme <tewodrosmeshesha@cbe.com.et>
2022-10-31 21:53:34 +0300

    All

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
 
-	for (i = 0; i < n; i++)
-		*destination[i] = *source[i];
-
-	return (*destination);
+    for (i = 0; i < n; i++)
+    {
+        dest[i] = src[i];
+    }
+	return (dest);
 }
