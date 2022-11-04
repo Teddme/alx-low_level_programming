7dbac786d6df9cdd14f84aa4c0015258793a1259
Teddme <tewodrosmeshesha@cbe.com.et>
2022-11-04 21:20:39 +0300

    arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prt ex. prg name
+ * @argc: # of arg
+ * @argv: array of arg
+ * Return: rslt.
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
