d20e3fdb3e7fa93063ec0157c154ef83a7815082
teddme <tewodrosmeshesha@cbe.com.et>
2022-12-04 21:58:06 +0300

    nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _isalpha - ck input is alphabet
- * @c: input str
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
  *
- * Return: 1 if alp and 0 if other
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
 int _isalpha(int c)
 {
